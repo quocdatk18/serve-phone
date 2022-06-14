bc379c5cb00e203ed94d1b0f2b00d2ce5c4dd3f1
unknown <quocdatlop109@gmaail.com>
2022-06-14 12:47:34 +0700

    create

---
+++ config/db/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+async function connectDB() {
+    const url = 'mongodb://127.0.0.1:27017/shop'
+    // const url = 'mongodb+srv://CaoKhaHieu:<CaoKhaHieu>@cluster0.r9hva.mongodb.net/shop?retryWrites=true&w=majority'
+    try {
+        await mongoose.connect(process.nv.MONGODB_URL || url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+        console.log("connected to db")
+    } catch (error) {
+        throw new DbConnectionError();
+        console.log(error)
+    }
+}
+
+export default connectDB;
